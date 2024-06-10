68b93e26be779b26f5d78a61a6b7ea55346195be
Neirel <neirelzapatos@gmail.com>
2024-06-10 14:41:30 -0700

    Chapter 14

---
+++ 14 JavaScript/notes.js
@@ -0,0 +1,35 @@
+alert("Hello world!");
+2 + 3;
+
+alert(2 + 3);
+
+typeof(23);
+
+var myName = prompt("Whats your name");
+alert("My name is" + myName);
+myName.length;
+myName.slice(0, 1); 
+// This prints the first letter
+var higher = yName.toUpperCase();
+var lower = myName.toLowerCase();
+// % gives remainder of division for example 9 % 6 = 3
+
+alert("Dog Age: " + ((dogAge - 2) * 4 + 21)) 
+// For reference
+
+var x = 3;
+var y = x++;
+// y = 3 because y is assigned to the value of x before x is incremented
+
+function getMilk(){
+    console.log("hello");
+    // logs the string inside the console
+}
+
+getMilk();
+// calls funciton
+
+function getMilk(bottles) {
+    console.log(bottles);
+    return bottles;
+}
