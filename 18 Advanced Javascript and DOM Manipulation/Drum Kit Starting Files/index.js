d276c0987c44144da5bc102303ba56fe15f3aa28
Neirel <neirelzapatos@gmail.com>
2024-06-20 11:26:54 -0700

    finished drum

--- 18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ 18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -1,53 +1,64 @@
-// var audio =  new Audio('./sounds/tom-1.mp3');
+function checkSound(key) {
+    switch(key){
+        case "w":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default:
+            console.log(key);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
     //query Select returns a node list aka an array
 
     // this adds an event listener to every element in the array
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        // this.style.color = "white";
-        // console.log(this.innerHTML);
-        // instructions
-
-        switch(this.innerHTML){
-            case "w":
-                var crash = new Audio("./sounds/crash.mp3");
-                crash.play();
-                break;
-            case "a":
-                var kick = new Audio("./sounds/kick-bass.mp3");
-                kick.play();
-                break;
-            case "s":
-                var snare = new Audio("./sounds/snare.mp3");
-                snare.play();
-                break;
-            case "d":
-                var tom1 = new Audio("./sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case "j":
-                var tom2 = new Audio("./sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            case "k":
-                var tom3 = new Audio("./sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case "l":
-                var tom4 = new Audio("./sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            default:
-                console.log(this.innerHTML);
-        }
-
+        checkSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+        console.log(this.classList)
     }); 
 }
 
+document.addEventListener("keydown", function(event) {
+    checkSound(event.key);
+    buttonAnimation(event.key);
+});     
 
 
 
-
-audio.play();
-alert("I got clicked!");
+// audio.play();
+// alert("I got clicked!");
--- 18 Advanced Javascript and DOM Manipulation/notes.js
+++ 18 Advanced Javascript and DOM Manipulation/notes.js
@@ -12,3 +12,26 @@ function calculator(num1, num2, operator){
 
 console.log(calculator(4, 5, add));
 // you can pass funcitons into other functions. This is called higher order funcitons
+
+// creating a javascript object
+var housekeeper1 = {
+    name: "Alyssa",
+    age: 28,
+    hasPermit: true
+}
+
+console.log(housekeeper1);
+
+// creating an object factory
+function BellBoy(name, age, hasPermit)  {
+    this.name = name;
+    this.age = age;
+    this.hasPermit = hasPermit;
+    this.clean = function() { 
+        console.log("cleaning");
+        alert("TESTING");
+    }
+}
+
+bellBoy1 = new BellBoy("David", 18, false);
+console.log(bellBoy1.clean());
