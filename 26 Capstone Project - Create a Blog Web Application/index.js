2691920515ef176fab7f096b0176e14ab30d66e9
Neirel <neirelzapatos@gmail.com>
2024-06-26 22:45:02 -0700

    finished back end

--- 26 Capstone Project - Create a Blog Web Application/index.js
+++ 26 Capstone Project - Create a Blog Web Application/index.js
@@ -12,11 +12,15 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// home page
 app.get("/", (req, res) => {
     console.log(`Connecting to port ${port}`);
-    res.render(__dirname + "/views/index.ejs");
+    res.render(__dirname + "/views/index.ejs", {
+        posts: blog
+    });
 });
 
+// creating new posts
 app.post("/submit", (req, res) => {
     console.log("Submitting new post");
     blog.push({
@@ -24,9 +28,10 @@ app.post("/submit", (req, res) => {
         message: req.body["message"]
     });
     console.log(blog);
-    res.render(__dirname + "/views/index.ejs", { posts : blog });
+    res.redirect("/");
 });
 
+// page to update posts
 app.get("/update-page/:index", (req, res) => {
     res.render(__dirname + "/views/update-page.ejs", {
         posts: blog,
@@ -34,8 +39,26 @@ app.get("/update-page/:index", (req, res) => {
     });
 });
 
-app.post("/update", (req, res) => {
+// page to view post
+app.get("/view-page/:index", (req, res) => {
+    res.render(__dirname + "/views/view-page.ejs", {
+        posts: blog,
+        postIndex: req.params.index
+    });    
+});
 
+// updates posts
+app.post("/update/:index", (req, res) => {
+    blog[req.params.index]["title"] = req.body["newTitle"];
+    blog[req.params.index]["message"] = req.body["newMessage"];
+    res.redirect("/");
+});
+
+// deletes a post
+app.get("/delete/:index", (req, res) => {
+    blog.splice(req.params.index);
+    console.log(blog);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
