6baeeb95dfde697378a47f33373cdc018c325a1d
Neirel Zapatos <neirelzapatos@gmail.com>
2024-08-13 14:20:39 -0700

    learning mapping

--- 36 React.js/298-mapping-components/src/components/Card.jsx
+++ 36 React.js/298-mapping-components/src/components/Card.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Avatar from "./Avatar";
-import Details from "./Details";
+import Detail from "./Detail";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        <p>{props.id}</p>
         <h2 className="name">{props.name}</h2>
-        <Avatar img={props.img}/>
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <Details info={props.tel} />
-        <Details info={props.email} />
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
       </div>
     </div>
   );
