2f74b5f6da85d24d0b9a43f020768fc44be76260
Neirel Zapatos <neirelzapatos@gmail.com>
2024-08-14 23:18:11 -0700

    complex state practice

---
+++ 36 React.js/314-changing-complex-state-practice/src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function updateContact(event) {
+    const { name, value} = event.target;
+
+    setContact(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" value={contact.fName} onChange={updateContact} />
+        <input name="lName" placeholder="Last Name" value={contact.lName} onChange={updateContact} />
+        <input name="email" placeholder="Email" value={contact.email} onChange={updateContact} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
