6b0678946a9715903ec4e9a947995b12b0bf5bd4
Neirel Zapatos <neirelzapatos@gmail.com>
2024-08-14 17:19:33 -0700

    hooks vs classes

--- 36 React.js/311-react-forms/src/components/App.jsx
+++ 36 React.js/311-react-forms/src/components/App.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [realName, setRealName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setRealName(name);
+    // this prevents the page from reloading after clicking submit button
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {realName}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+    </div>
+  );
+
+  // way without html form tag
+  return (
+    <div className="container">
+      <h1>Hello {realName}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
